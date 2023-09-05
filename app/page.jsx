43ae23f3df350498bf28e2543a40db8797261317
thomas allen <thomasallenmartinho@gmail.com>
2023-09-05 20:26:56 +0200

    started recipe show page

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,29 @@
 import Recipes from "@/components/Recipes";
 import styles from "../styles/home.module.scss";
 import Calendar from "@/components/HomeCalendar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Welcome to Foodie Planner</h1>
-      <Recipes />
-      <Calendar />
+      <div className={styles.banner}>
+        <h1>Welcome to Foodie Planner</h1>
+        <Image
+          priority
+          src="/images/foodBanner.jpg"
+          alt="foodie planner's banner image"
+          width={1024}
+          height={596}
+          id={styles.bannerImage}
+        />
+      </div>
+      <div className={styles.recipes}>
+        <h2>Browse through our most liked recipes...</h2>
+        <Recipes />
+      </div>
+      <div className={styles.calendar}>
+        <Calendar />
+      </div>
     </main>
   );
 }
--- app/recipe/[recipeId]/page.jsx
+++ app/recipe/[recipeId]/page.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+'use client'
 
-const Page = () => {
+import { useState, useEffect } from 'react';
+
+async function fetchRecipe(id) {
+  const res = fetch(`http://localhost:3000/api/v1/recipes/${id}`)
+  console.log(res.json())
+  return res.json()
+}
+
+export default function Page ({ params }) {
+  const [recipe, setRecipe] = useState([])
+
+  useEffect(() => {
+    // console.log(params.recipeId)
+    async function getRecipe() {
+      const myRecipe = await fetchRecipe(params.recipeId)
+      setRecipe(myRecipe)
+      // console.log(myRecipe)
+    }
+    // console.log(recipe);
+  }, []);
+
   return (
     <div>
       Hello from Recipe show page
     </div>
   );
 }
-
-export default Page;
