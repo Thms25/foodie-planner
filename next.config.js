a00fd59f2f54279b6bdfa6fa685e100847c47f5e
thomas allen <thomasallenmartinho@gmail.com>
2023-10-04 18:51:25 +0200

    it is fixed oh my

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    // loader: "cloudinary",
+    // reactStrictMode: true,
     domains: ["res.cloudinary.com"],
+    // path: "https://res.cloudinary.com/<cloudinary-id>/image/upload",
   },
 };
 
