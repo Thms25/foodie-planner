eb583e2e0b7e0e4e6a0c09324ccbefac052731db
thomas allen <thomasallenmartinho@gmail.com>
2023-12-07 20:08:00 +0100

    crud almost complete for recipes

--- app/api/recipe/route.js
+++ app/api/recipe/route.js
@@ -9,7 +9,6 @@ export async function GET(request) {
     await connectToDb();
 
     const recipes = await Recipe.find({});
-    console.log(recipes);
 
     return new Response(JSON.stringify(recipes), { status: 201 });
   } catch (error) {
